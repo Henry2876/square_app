4e99ab54915f2d495a656ad185f620cee96eec1f
Henry2876 <henrywwwalker@gmail.com>
2025-04-02 20:07:27 +0300

    Create screen
    Layout screen using StatelessWidget

---
+++ lib/screens/screen_home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../utils/translations.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(homeScreenName),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(homeScreenName),
+      ),
+    );
+  }
+}
