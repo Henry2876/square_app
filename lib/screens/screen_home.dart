92c4495127e62bd613a796f4b8d45df56d579ae0
Henry2876 <henrywwwalker@gmail.com>
2025-04-03 16:53:39 +0300

    Create screen and build design
    Remove and add file for optimize app

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:square_app/utils/colors.dart';
+import 'package:square_app/utils/sizes.dart';
 import '../utils/translations.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,8 +13,159 @@ class HomeScreen extends StatelessWidget {
         title: const Text(homeScreenName),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(homeScreenName),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: colorSquare_1,
+          ),
+          FractionallySizedBox(
+            widthFactor: size_0750,
+            child: AspectRatio(
+              aspectRatio: size_1,
+              child: Container(
+                color: colorSquare_2,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: FractionallySizedBox(
+                    widthFactor: size_0990,
+                    child: AspectRatio(
+                      aspectRatio: size_1,
+                      child: Container(
+                        color: colorSquare_3,
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0180,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_4,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0175,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_5,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0180,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_6,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0175,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_7,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0180,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_8,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0175,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_9,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0180,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_10,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0175,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_11,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0090,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_12,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: FractionallySizedBox(
+                                widthFactor: size_0088,
+                                child: AspectRatio(
+                                  aspectRatio: 1.0,
+                                  child: Container(
+                                    color: colorSquare_13,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+const Color colorSquare_1 = Color(0xFFFFFFFF);
+const Color colorSquare_2 = Color(0xFF2A42FF);
+const Color colorSquare_3 = Color(0xFFFF0000);
+const Color colorSquare_4 = Color(0xACD1C9FF);
+const Color colorSquare_5 = Color(0xFF8FC404);
+const Color colorSquare_6 = Color(0xFF91C0CE);
+const Color colorSquare_7 = Color(0xFF2DACE0);
+const Color colorSquare_8 = Color(0xFFB5B9BD);
+const Color colorSquare_9 = Color(0xFFA05BC4);
+const Color colorSquare_10 = Color(0xFFA4AFB0);
+const Color colorSquare_11 = Color(0xFF020202);
+const Color colorSquare_12 = Color(0xFFBDB9B6);
+const Color colorSquare_13 = Color(0xFFFE7C04);
+
