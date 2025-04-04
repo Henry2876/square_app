63fe675a7f28fe1dee7b0660c41b740245165bb0
Henry2876 <henrywwwalker@gmail.com>
2025-04-04 14:14:27 +0300

    Fix color screen
    Handle backgroundColor in Scaffold

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(homeScreenName),
         centerTitle: true,
